5a41eb421eb2c981957e155da7f80ab211054b4e
David Fitzgerald <davidfit@cisco.com>
2021-10-08 20:04:07 +0100

    Clearer explanation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-# This script copies everything from the current directory (except venv and requirements.txt) to the ubuntu server into
-# the ~/Documents/BAT_RAT directory
+# This script copies everything from the current directory (except venv and requirements.txt) to the ~/Documents/BAT_RAT
+# directory on the Ubuntu server.
 echo "Copying everything except venv/ and requirements.txt from $(pwd) to rattool@10.52.247.154:~/Documents/BAT_RAT/"
 scp -r ./[!venv][!requirements.txt]* rattool@10.52.247.154:~/Documents/BAT_RAT/
 
