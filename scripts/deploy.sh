c837d4804ccdc3e59d09405da9a8ae6586ca879b
David Fitzgerald <davidfit@cisco.com>
2021-10-08 20:22:37 +0100

    Change deployment script to include requirements.txt now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 # This script copies everything from the current directory (except venv and requirements.txt) to the ~/Documents/BAT_RAT
 # directory on the Ubuntu server.
-echo "Copying everything except venv/ and requirements.txt from $(pwd) to rattool@10.52.247.154:~/Documents/BAT_RAT/"
-scp -r ./[!venv][!requirements.txt]* rattool@10.52.247.154:~/Documents/BAT_RAT/
+echo "Copying everything except venv/ from $(pwd) to rattool@10.52.247.154:~/Documents/BAT_RAT/"
+scp -r ./[!venv]* rattool@10.52.247.154:~/Documents/BAT_RAT/
 
